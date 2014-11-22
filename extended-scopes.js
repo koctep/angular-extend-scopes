cec457dcc9a6d448c6e82a2efa1eba05ba1d6061
Ilya Ashchepkov <koctep@gmail.com>
2014-11-22 13:57:38 +0600

    watcherExp moved to separate package

--- extended-scopes.js
+++ extended-scopes.js
@@ -55,21 +55,4 @@ angular.module('extended-scopes', [])
     };
   }];
 }])
-.service('watcherExp', [function() {
-  return function watcherExp(watcher) {
-    if (!watcher.exp) throw('watcherExp: no exp');
-    if (!angular.isFunction(watcher.exp)) {
-      if (watcher.parts) {
-        if (watcher.parts[1]) {
-          throw ('watcherExp: to many parts');
-        } else {
-          return watcher.parts[0].exp;
-        }
-      } else {
-        return watcher.exp;
-      }
-    }
-    return watcherExp(watcher.exp);
-  }
-}])
 ;
